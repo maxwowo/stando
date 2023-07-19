2d8b8d1e398ce01a35f17b29b0e93db77c57ce1d
Max Wo <hello@maxwowo.com>
2023-07-19 20:19:05 +1200

    Create progress and session models

--- Stando/StandoApp.swift
+++ Stando/StandoApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct StandoApp: App {
+    @State private var isSitting = true
+    @State private var isPaused = false
+    @State private var duration = 0
+    
     var body: some Scene {
         MenuBarExtra("Stando", image: isSitting ? "figure.seated.side" : "figure.stand") {
             PopoverView(isSitting: $isSitting)
